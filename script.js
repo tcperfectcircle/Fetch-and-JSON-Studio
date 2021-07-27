c5208b40384686adecb6c85ad420643f01ce6c8f
Your Name <you@example.com>
2021-07-26 19:47:43 -0500

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 // TODO: add code here
+window.addEventListener('load', function(){
+
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json")
+    .then(function(response){
+        response.json().then(function(json){
+        let myCrewDiv = document.getElementById('container');
+
+            for (let i = 0; i < json.length; i++){
+            myCrewDiv.innerHTML += `
+            <div class = "astronaut"
+                <div vlass = "bio">
+                    <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                    <ul>
+                    <li>${json[i].hoursInSpace}</li>
+                    <li>${json[i].active}</li>
+                    <li>${json[i].skills}</li>
+                    </ul>
+                    <img class = "avatar" src="${json[i].picture}">
+                </div>
+            </div>
+            `
+            }
+
+        });
+
+    });
+
+
+});
